79b437b64bd40b883799181d9b15589b252736bb
Achintya Jaimini <45306167+Achintya-Jaimini@users.noreply.github.com>
2025-08-13 18:02:31 -0700

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home | Cube Solvers</title>
+  <link rel="icon" href="unnamed.png" type="image/png">
+  <link rel="stylesheet" type="text/css" href="styles.css" media="screen">
+
+</head>
+<body>
+<header>
+  <hgroup>
+    <h1 class="hello">Cube Solvers</h1>
+    <h2>website for Rubik's Cube solving tutorials</h2>
+  </hgroup>
+  
+  <nav>
+    <ul>
+	  <li><a href="index.html"><button>Home</button></a></li>
+	  <li><a href="about.html"><button>About</button></a></li>
+	  <li><a href="contact.html"><button>Contact</button></a></li>
+    </ul>
+  </nav>
+</header>
+<section>
+<div class="php">
+<p>Thank You</p><b><?php echo $_POST["yourname"]; ?></b><br>
+<p>we will surely get you on this email:</p><b>
+<?php echo $_POST["youremail"]; ?></b>
+</div>
+</section>
+<footer>
+
+<small>Copyright © 2021 Achintya Jaimini</small>
+
+  <nav>
+    <ul>
+	  <li><a href="index.html"><button>Home</button></a></li>
+	  <li><a href="about.html"><button>About</button></a></li>
+	  <li><a href="contact.html"><button>Contact</button></a></li>
+    </ul>
+  </nav>
+
+</footer>
+</body>
+</html>
